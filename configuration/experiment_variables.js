04287cfd8b439cd7c53a4f2fa12d1a937f2b711b
Keith Kreschollek <kck225@lehigh.edu>
2024-07-09 15:58:18 -0400

    Eye tracking heatmap barely works but I understand how to make it work eventually

--- configuration/experiment_variables.js
+++ configuration/experiment_variables.js
@@ -57,7 +57,7 @@ var NdesignReps_exp = 1;
 // Number of experimental blocks (excluding the first practice block).
 // Note that NexpBl = 0 will still run the practice block
 
-var NexpBL = 1;
+var NexpBL = 0;
 
 
 // ----- CUSTOMISE THE TIME INTERVALS (in milliseconds)-----
